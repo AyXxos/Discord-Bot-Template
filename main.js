99ba5c82de78cdb82feba6823b9a500990af44fe
Adam Sicaud <adamsicaud@gmail.com>
2025-09-02 11:34:44 +0200

    Infomations added

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const loadEvents = require("./Loaders/loadEvents");
 require('dotenv').config();
 bot.commands = new Discord.Collection();
 
+console.log("Bot is starting...");
+
 bot.login(process.env.DISCORD_TOKEN);
 loadCommands(bot);
 loadEvents(bot);
